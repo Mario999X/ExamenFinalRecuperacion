ba3f7aa17484b421a0c8655d7b4e1e97862b6b85
Mario999X <mariogr901@gmail.com>
2022-06-13 19:25:04 +0200

    Eliminar y editar empleados terminado.

--- app/src/main/java/com/example/examenfinalrecuperacion/fragments/VerEmpleado.java
+++ app/src/main/java/com/example/examenfinalrecuperacion/fragments/VerEmpleado.java
@@ -1,6 +1,7 @@
 package com.example.examenfinalrecuperacion.fragments;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 
@@ -11,8 +12,17 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.examenfinalrecuperacion.R;
 import com.example.examenfinalrecuperacion.data.DataRoomDB;
@@ -26,6 +36,8 @@ public class VerEmpleado extends Fragment {
     TextView textViewNombreDetalle, textViewIdDetalle, textViewLugarDetalle, textViewDepartamentoDetalle;
 
     DataRoomDB database;
+
+    String opcionSpinner, opcionLugarTrabajo;
 
     EmpleadoEntity empleadoElegido;
 
@@ -52,8 +64,7 @@ public class VerEmpleado extends Fragment {
         textViewLugarDetalle = v.findViewById(R.id.textViewLugarDetalle);
         textViewDepartamentoDetalle = v.findViewById(R.id.textViewDepartamentoDetalle);
 
-        // SET INFO EMPLEADO
-
+        // SET INFO EMPLEADO ELEGIDO
         imageViewDetalle.setImageResource(empleadoElegido.getFoto());
         textViewNombreDetalle.setText(empleadoElegido.getNombreCompleto());
         textViewIdDetalle.setText("ID: " + empleadoElegido.getId());
@@ -69,7 +80,104 @@ public class VerEmpleado extends Fragment {
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.menuEditar:
-                        System.out.println("Click");
+                        // OBTENEMOS LOS DATOS EXISTENTES DEL EMPLEADO SELECCIONADO
+                        int sId = empleadoElegido.getId();
+                        String sNombre = empleadoElegido.getNombreCompleto();
+                        //String sDepartamento = empleadoElegido.getDepartamento();
+                        //String sLugarTrabajo = empleadoElegido.getLugarTrabajo();
+
+                        // CREACION DIALOGO
+                        Dialog dialog = new Dialog(getContext());
+                        dialog.setContentView(R.layout.dialog_editar_empleado);
+
+                        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+                        lp.copyFrom(dialog.getWindow().getAttributes());
+                        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+                        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+                        dialog.show();
+                        dialog.getWindow().setAttributes(lp);
+
+                        // ELEMENTOS DIALOGO
+                        EditText editTextNombreEditar = dialog.findViewById(R.id.editTextNombreEditar);
+                        Spinner spinnerDepartamentosEditar = dialog.findViewById(R.id.spinnerDepartamentosEditar);
+                        RadioGroup radioGroupLugarTrabajo = dialog.findViewById(R.id.radioGroupLugarTrabajoEditar);
+                        Button btnEditarEmpleado = dialog.findViewById(R.id.btnEditarEmpleado);
+
+                        // Preparar & Rellenar Spinner
+                        String[] departamentos = new String[]{"Atención al cliente", "Reparaciones", "Marketing", "Dependientes"};
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, departamentos);
+                        spinnerDepartamentosEditar.setAdapter(adapter);
+
+                        spinnerDepartamentosEditar.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                            @Override
+                            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                                opcionSpinner = adapterView.getItemAtPosition(i).toString();
+                            }
+
+                            @Override
+                            public void onNothingSelected(AdapterView<?> adapterView) {
+
+                            }
+                        });
+
+                        // Preparar RadioGroup + Listener
+                        // Nos interesa el valor texto de cada uno, asi que tendremos que declarar los radioButton
+                        RadioButton radioButton5 = dialog.findViewById(R.id.radioButton5);
+                        RadioButton radioButton6 = dialog.findViewById(R.id.radioButton6);
+                        RadioButton radioButton7 = dialog.findViewById(R.id.radioButton7);
+                        RadioButton radioButton8 = dialog.findViewById(R.id.radioButton8);
+
+                        // Para evitar el valor NULL
+                        opcionLugarTrabajo = radioButton5.getText().toString();
+
+                        radioGroupLugarTrabajo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                                switch (i){
+                                    case R.id.radioButton5:
+                                        opcionLugarTrabajo = radioButton5.getText().toString();
+                                        break;
+                                    case R.id.radioButton6:
+                                        opcionLugarTrabajo = radioButton6.getText().toString();
+                                        break;
+                                    case R.id.radioButton7:
+                                        opcionLugarTrabajo = radioButton7.getText().toString();
+                                        break;
+                                    case R.id.radioButton8:
+                                        opcionLugarTrabajo = radioButton8.getText().toString();
+                                }
+                            }
+                        });
+
+                        // SET INFO EMPLEADO EN LOS CAMPOS POSIBLES... no se me ocurre como dejar marcados el spinner y el radiogroup.
+                        editTextNombreEditar.setText(sNombre);
+
+                        // LISTENER BOTON EDITAR
+                        btnEditarEmpleado.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // Obtener modificaciones y actualizar
+                                String uText = editTextNombreEditar.getText().toString();
+
+                                database.empleadoDao().updateNombre(sId, uText);
+                                database.empleadoDao().updateDepartamento(sId, opcionSpinner);
+                                database.empleadoDao().updateLugarTrabajo(sId, opcionLugarTrabajo);
+
+                                // Actualizar Vista Detallada
+                                imageViewDetalle.setImageResource(empleadoElegido.getFoto());
+                                textViewNombreDetalle.setText(uText);
+                                textViewIdDetalle.setText("ID: " + empleadoElegido.getId());
+                                textViewLugarDetalle.setText(opcionLugarTrabajo);
+                                textViewDepartamentoDetalle.setText(opcionSpinner);
+
+                                Toast.makeText(getActivity(), "Empleado Editado", Toast.LENGTH_SHORT).show();
+                                // Cerrar dialogo
+                                dialog.dismiss();
+
+                            }
+                        });
                         break;
                     case R.id.menuEliminar:
                         // Gracias StackOverflow
@@ -78,7 +186,10 @@ public class VerEmpleado extends Fragment {
                             public void onClick(DialogInterface dialog, int which) {
                                 switch (which){
                                     case DialogInterface.BUTTON_POSITIVE:
+                                        database.empleadoDao().delete(empleadoElegido);
                                         dialog.dismiss();
+                                        Toast.makeText(getActivity(), "Empleado Eliminado", Toast.LENGTH_SHORT).show();
+                                        getFragmentManager().popBackStackImmediate();
                                         break;
 
                                     case DialogInterface.BUTTON_NEGATIVE:
